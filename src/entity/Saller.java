7f42a4a077af6b78f493fcd96f75db2f7c5cc670
AngeloModanez <angelomodanez30@gmail.com>
2024-10-21 16:32:43 -0300

    :sparkles: feat: Saller Entity

---
+++ src/entity/Saller.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class Saller {
+    private int SallerID;
+    private String Saller;
+
+    public int getSallerID() {
+        return SallerID;
+    }
+
+    public void setSallerID(int sallerID) {
+        SallerID = sallerID;
+    }
+
+    public String getSaller() {
+        return Saller;
+    }
+
+    public void setSaller(String saller) {
+        Saller = saller;
+    }
+}
